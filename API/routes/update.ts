556e2fba33b56b87875610bca213613fb5a745bc
Liran Piade <liranpiade@gmail.com>
2021-02-04 03:28:23 +0000

    Attempt to verify github webhooks

--- API/routes/update.ts
+++ API/routes/update.ts
@@ -1,7 +1,12 @@
 import { RouterContext } from "https://deno.land/x/oak@v6.3.1/router.ts";
-
+import { hmac } from "https://deno.land/x/hmac@v2.0.1/mod.ts";
 
 export const update = async (ctx: RouterContext) => {
     const body = await ctx.request.body().value;
-    console.log(body);
+    const gitHash = ctx.request.headers.get('X-Hub-Signature-256');
+    const secret = Deno.readTextFileSync(Deno.env.get('SECRET')!).trimEnd();
+    const bodyHash = hmac('sha256', secret, JSON.stringify(body), undefined, 'hex');
+    if (`sha256=${bodyHash}` === gitHash)
+    ctx.response.status = 200;
+    else ctx.response.status = 401;
 }
