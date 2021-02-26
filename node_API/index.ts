5d242b1ef243fb15e761fb1f1d6454300c011055
Liran Piade <liranpiade@gmail.com>
2021-02-26 10:32:51 +1000

    Allowing CORS access in the API

--- node_API/index.ts
+++ node_API/index.ts
@@ -2,6 +2,7 @@ import Koa from 'koa';
 import koaBody from 'koa-body';
 import serve from 'koa-static-server';
 import { router } from './router';
+import cors from "@koa/cors";
 const port = process.env.API_PORT ? parseInt(process.env.API_PORT) : 7700;
 
 const Application = new Koa();
@@ -13,6 +14,7 @@ Application
         await next();
         console.log(`Responded to request in ${Date.now() - startTime}ms (${ctx.response.status})`);
     })
+    .use(cors())
     .use(koaBody())
     .use(router.routes())
     .use(router.allowedMethods())
