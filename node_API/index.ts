3d98212d62e3290b71f74f0b8aed60305a1829f4
Liran Piade <liranpiade@gmail.com>
2021-03-09 06:04:57 +0000

    Revert "Allowing CORS access in the API"
    This reverts commit 5d242b1ef243fb15e761fb1f1d6454300c011055.

--- node_API/index.ts
+++ node_API/index.ts
@@ -2,7 +2,6 @@ import Koa from 'koa';
 import koaBody from 'koa-body';
 import serve from 'koa-static-server';
 import { router } from './router';
-import cors from "@koa/cors";
 const port = process.env.API_PORT ? parseInt(process.env.API_PORT) : 7700;
 
 const Application = new Koa();
@@ -14,7 +13,6 @@ Application
         await next();
         console.log(`Responded to request in ${Date.now() - startTime}ms (${ctx.response.status})`);
     })
-    .use(cors())
     .use(koaBody())
     .use(router.routes())
     .use(router.allowedMethods())
