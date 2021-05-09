2b6465579ba3cae2d14c2b626538c0e52d5014c8
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2021-05-09 19:36:27 +1000

    Attempted fix.

--- webapp/src/pages/algorithms/147Cipher.tsx
+++ webapp/src/pages/algorithms/147Cipher.tsx
@@ -47,12 +47,15 @@ const Cipher = (props: { state: appState }) => {
   const baseOptions = ['Base16', 'Base32', 'Base64', 'Base85'];
 
   const send = async (action: string) => {
-    const res = await sendInput(action, "147cipher", result, loading.set, {
+    const body: any = {
       formatting: base,
       in_text: content.value,
-      key: factor.value,
-      ...nonce ? { nonce_type: nonce } : {}
-    });
+      key: factor.value
+    };
+    if (action === 'encrypt') {
+      body.nonce_type = nonce
+    }
+    const res = await sendInput(action, "147cipher", result, loading.set, body);
     resLabel.value = `${capitalise(action)}ed result`;
     if (res?.response.ok)
       result.value = res?.result!;
